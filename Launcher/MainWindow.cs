60b87f36f1d47a52c490b90a4c7cce8d388f411b
cra0zy <cra0zy@gmail.com>
2017-02-05 17:36:43 +0100

    Reenable borders

--- Launcher/MainWindow.cs
+++ Launcher/MainWindow.cs
@@ -6,38 +6,9 @@ namespace Launcher
 {
     public partial class MainWindow : Form, IView
     {
-        private bool _move;
-        private PointF _mousePosition;
-
         public MainWindow()
         {
             CreateContent();
-
-            _move = false;
-            _mousePosition = new PointF();
-        }
-
-        private void PanelTitlebar_MouseDown(object sender, MouseEventArgs e)
-        {
-            _mousePosition = e.Location;
-            _move = true;
-        }
-
-        private void PanelTitlebar_MouseUp(object sender, MouseEventArgs e)
-        {
-            _move = false;
-        }
-
-        private void PanelTitlebar_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (_move)
-            {
-                var newx = (int)(Location.X + e.Location.X - _mousePosition.X);
-                var newy = (int)(Location.Y + e.Location.Y - _mousePosition.Y);
-
-                if (Location.X != newx || Location.Y != newy)
-                    Location = new Point(newx, newy);
-            }
         }
 
         private void _webview1_DocumentTitleChanged(object sender, WebViewTitleEventArgs e)
--- Launcher/MainWindow.eto.cs
+++ Launcher/MainWindow.eto.cs
@@ -6,7 +6,6 @@ namespace Launcher
     public partial class MainWindow : Form
     {
         private Panel _panelTitlebar;
-        private PixelLayout _pixel1;
         private WebView _webview1;
 
         private void CreateContent()
@@ -16,24 +15,13 @@ namespace Launcher
             Width = 900;
             Height = 500;
             Resizable = false;
-            WindowStyle = WindowStyle.None;
-
-            _pixel1 = new PixelLayout();
+            //WindowStyle = WindowStyle.None;
 
             _webview1 = new WebView();
             _webview1.Width = Width;
             _webview1.Height = Height;
-            _pixel1.Add(_webview1, 0, 0);
 
-            _panelTitlebar = new Panel();
-            _panelTitlebar.Width = Width - 2 * (16 + 5) - 10;
-            _panelTitlebar.Height = 30;
-            _panelTitlebar.MouseDown += PanelTitlebar_MouseDown;
-            _panelTitlebar.MouseUp += PanelTitlebar_MouseUp;
-            _panelTitlebar.MouseMove += PanelTitlebar_MouseMove;
-            _pixel1.Add(_panelTitlebar, 0, 0);
-
-            Content = _pixel1;
+            Content = _webview1;
         }
     }
 }
